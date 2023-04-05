623e4648f9afc503d85f17c2c1b99e7e2ad9ebed
Aleksandra Petrova <petrowa.alex@yandex.com>
2023-04-05 14:18:27 +0300

    fix: fixed problem with data source, now app get data from model, not from separate file

--- lib/models/sound_model.dart
+++ lib/models/sound_model.dart
@@ -10,52 +10,52 @@ class SoundModel extends ChangeNotifier {
         link: 'audio/data_resources_sounds_storm.ogg',
         icon: WeatherIcons.storm_showers,
         image: Image.asset(
-          'assets/img/icons8-default.png',
+          'assets/img/icons8-thunderstorm.png',
         )),
     'Birds': Sound(
       link: 'audio/data_resources_sounds_birds.ogg',
       icon: Icons.music_note,
-      image: Image.asset('assets/img/icons8-default.png'),
+      image: Image.asset('assets/img/icons8-birds.png'),
     ),
     'City': Sound(
       link: 'audio/data_resources_sounds_city.ogg',
       icon: Icons.location_city,
-      image: Image.asset('assets/img/icons8-default.png'),
+      image: Image.asset('assets/img/icons8-city.png'),
     ),
     'Coffee-shop': Sound(
       link: 'audio/data_resources_sounds_coffee-shop.ogg',
       icon: Icons.coffee_maker,
-      image: Image.asset('assets/img/icons8-default.png'),
+      image: Image.asset('assets/img/icons8-coffee-shop.png'),
     ),
     'Fireplace': Sound(
       link: 'audio/data_resources_sounds_fireplace.ogg',
       icon: Icons.fireplace,
-      image: Image.asset('assets/img/icons8-default.png'),
+      image: Image.asset('assets/img/icons8-fire.png'),
     ),
     'Wind': Sound(
       link: 'audio/data_resources_sounds_wind.ogg',
       icon: WeatherIcons.wind_direction,
-      image: Image.asset('assets/img/icons8-default.png'),
+      image: Image.asset('assets/img/icons8-wind.png'),
     ),
     'Train': Sound(
       link: 'audio/data_resources_sounds_train.ogg',
       icon: Icons.train,
-      image: Image.asset('assets/img/icons8-default.png'),
+      image: Image.asset('assets/img/icons8-train.png'),
     ),
     'Summer night': Sound(
       link: 'audio/data_resources_sounds_summer-night.ogg',
       icon: WeatherIcons.night_alt_lightning,
-      image: Image.asset('assets/img/icons8-default.png'),
+      image: Image.asset('assets/img/icons8-half-moon.png'),
     ),
     'Waves': Sound(
       link: 'audio/data_resources_sounds_waves.ogg',
       icon: Icons.waves,
-      image: Image.asset('assets/img/icons8-default.png'),
+      image: Image.asset('assets/img/icons8-waves.png'),
     ),
   };
 
-  UnmodifiableListView<Sound> get soundsList =>
-      UnmodifiableListView(_soundsList as Iterable<Sound>);
+  UnmodifiableMapView<String, Sound> get soundsList =>
+      UnmodifiableMapView(_soundsList);
 
   // SoundModel(this._catalogueSounds);
 
