ccf2dd82b5de9661a57572c85eb5cba966bdc86c
Alexandra Petrova <96407924+leksidev@users.noreply.github.com>
2023-08-14 02:20:28 +0300

    feat: add router observer

--- lib/config/routes/routes.dart
+++ lib/config/routes/routes.dart
@@ -1,9 +1,10 @@
 import 'package:atmosphere/features/sounds_screen/view/sounds_screen.dart';
 import 'package:atmosphere/features/pressets/view/pressets_screen.dart';
-
 import 'package:atmosphere/widgets/scaffold_with_navbar.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 final GlobalKey<NavigatorState> _rootNavigatorKey =
     GlobalKey<NavigatorState>(debugLabel: 'root');
@@ -11,6 +12,11 @@ final GlobalKey<NavigatorState> _shellNavigatorKey =
     GlobalKey<NavigatorState>(debugLabel: 'shell');
 // GoRouter configuration
 final router = GoRouter(
+  observers: [
+    TalkerRouteObserver(
+      GetIt.I<Talker>(),
+    )
+  ],
   navigatorKey: _rootNavigatorKey,
   initialLocation: '/',
   routes: [
@@ -22,7 +28,7 @@ final router = GoRouter(
         routes: [
           GoRoute(
             path: '/',
-            builder: (context, state) => SoundsScreen(),
+            builder: (context, state) => const SoundsScreen(),
           ),
           GoRoute(
             path: '/pressets',
