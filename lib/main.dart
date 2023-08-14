c2cf970ae499d3c7a3d9e6ed04abe690b8754718
Alexandra Petrova <96407924+leksidev@users.noreply.github.com>
2023-08-14 12:09:47 +0300

    feat: plug Firebase Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:atmosphere/config/routes/routes.dart';
+import 'package:atmosphere/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
@@ -6,7 +10,19 @@ import 'package:talker_flutter/talker_flutter.dart';
 
 import 'models/playlist.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   final talker = TalkerFlutter.init();
   GetIt.I.registerSingleton(talker);
 
