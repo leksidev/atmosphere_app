74c0f94aba53cc77b8dffd78eeb4a0ed6c752fb7
Alexandra Petrova <96407924+leksidev@users.noreply.github.com>
2023-08-26 14:46:24 +0300

    feat: add links and text in infoscreen

--- lib/features/info/view/info_screen.dart
+++ lib/features/info/view/info_screen.dart
@@ -1,16 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class InfoScreen extends StatelessWidget {
   const InfoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        FlutterLogo(),
-        Text('''АТМОСФЕРА — приложение для комбинации фоновых звуков. 
-            Создайте шум, под который сможете расслабиться или сосредоточиться; 
-            сохраните настройки; управляйте воспроизведением из шторки уведомлений.''')
+        // const FlutterLogo(),
+        Text(
+            "АТМОСФЕРА — приложение для создания комбинаций из фоновых звуков для расслабления, медитайций, работы, учебы, сна.",
+            style: Theme.of(context).textTheme.bodyLarge),
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
+            "1. Создайте шум, под который сможете расслабиться или сосредоточиться.",
+            style: Theme.of(context).textTheme.bodyLarge),
+        Text("2. Сохраните настройки.",
+            style: Theme.of(context).textTheme.bodyLarge),
+        Text("3. Управляйте воспроизведением из шторки уведомлений.",
+            style: Theme.of(context).textTheme.bodyLarge),
+        const SizedBox(
+          height: 10,
+        ),
+        const Expanded(
+          child: SizedBox(),
+        ),
+        Row(
+          children: [
+            const Text("Иконки:"),
+            TextButton(
+              onPressed: () async {
+                final Uri url = Uri.parse('https://icons8.com');
+                if (!await launchUrl(url)) {
+                  throw Exception('Could not launch $url');
+                }
+              },
+              child: const Text('Icons8'),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            const Text("Звуки:"),
+            TextButton(
+              onPressed: () async {
+                final Uri url = Uri.parse('https://zvukipro.com/');
+                if (!await launchUrl(url)) {
+                  throw Exception('Could not launch $url');
+                }
+              },
+              child: const Text('zvukipro.com'),
+            ),
+          ],
+        ),
       ],
     );
   }
